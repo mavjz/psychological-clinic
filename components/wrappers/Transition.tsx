e15360bf4c156bc296a6228d8349d1f5fdabcf96
mavjz28 <maja.miaskiewicz@gmail.com>
2023-01-09 11:10:50 +0100

    Animation fix

---
+++ components/wrappers/Transition.tsx
@@ -0,0 +1,45 @@
+import React from "react";  
+import { WrapperType } from "types/sections/wrapper";
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
+
+const Transition = ({children}: WrapperType) => {
+    const { asPath } = useRouter();
+    const variants = {
+            out: {
+            opacity: 0,
+            y: 40,
+            transition: {
+                duration: 0.75
+            }
+        },
+        in: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.75,
+                delay: 0.5
+            }
+        }
+    };
+    return (
+        <React.Fragment>
+            <AnimatePresence
+                initial={false}
+                mode="wait"
+                >
+                <motion.div
+                    key={asPath}
+                    variants={variants}
+                    animate="in"
+                    initial="out"
+                    exit="out"
+                >
+                {children}
+                </motion.div>
+	        </AnimatePresence>
+        </React.Fragment>
+    )
+}
+
+export default Transition
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,14 @@
 import type { AppProps } from 'next/app'
 import "../styles/style.scss"
 import Layout from "components/wrappers/Layout"
-import { motion, AnimatePresence } from 'framer-motion';
-import { useRouter } from 'next/router';
+import Transition from 'components/wrappers/Transition'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { asPath } = useRouter();
-  const variants = {
-    out: {
-      opacity: 0,
-      y: 40,
-      transition: {
-        duration: 0.75
-      }
-    },
-    in: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.75,
-        delay: 0.5
-      }
-    }
-  };
   return (
     <Layout>
-      <AnimatePresence
-	      initial={false}
-	      exitBeforeEnter
-	    >
-	      <motion.div
-	        key={asPath}
-	        variants={variants}
-	        animate="in"
-	        initial="out"
-	        exit="out"
-	      >
-          <Component {...pageProps} />
-        </motion.div>
-	    </AnimatePresence>
+      <Transition>
+        <Component {...pageProps} />
+      </Transition>
     </Layout>
   )
 }
