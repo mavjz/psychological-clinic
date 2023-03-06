04092a02ab32baeda4a4c368317c26b8a79c4ddb
mavjz28 <maja.miaskiewicz@gmail.com>
2023-03-06 12:38:46 +0100

    sass colors + Headline

--- components/items/Headline.tsx
+++ components/items/Headline.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { HeadlineType } from 'types/items/headline'
 
 const Headline = ({h1, h2, h3, h4, h5, h6, text, place="textleft", color="textgreen", isUppercase}: HeadlineType) => {
+    color = "text".concat(color.toLowerCase());
+    place = "text".concat(place.toLowerCase());
+    const classes = color.concat(" "+place);
     return (
         <React.Fragment>
-            {h1 && <h1 className={`${place} ${color} ${isUppercase && "capslock"}`}>{text}</h1>}
-            {h2 && <h2 className={`${place} ${color} ${isUppercase && "capslock"}`}>{text}</h2>}
-            {h3 && <h3 className={`${place} ${color} ${isUppercase && "capslock"}`}>{text}</h3>}
-            {h4 && <h4 className={`${place} ${color} ${isUppercase && "capslock"}`}>{text}</h4>}
-            {h5 && <h5 className={`${place} ${color} ${isUppercase && "capslock"}`}>{text}</h5>}
-            {h6 && <h6 className={`${place} ${color} ${isUppercase && "capslock"}`}>{text}</h6>}
+            {h1 && <h1 className={`${classes} ${isUppercase && "capslock"}`}>{text}</h1>}
+            {h2 && <h2 className={`${classes} ${isUppercase && "capslock"}`}>{text}</h2>}
+            {h3 && <h3 className={`${classes} ${isUppercase && "capslock"}`}>{text}</h3>}
+            {h4 && <h4 className={`${classes} ${isUppercase && "capslock"}`}>{text}</h4>}
+            {h5 && <h5 className={`${classes} ${isUppercase && "capslock"}`}>{text}</h5>}
+            {h6 && <h6 className={`${classes} ${isUppercase && "capslock"}`}>{text}</h6>}
         </React.Fragment>
     )
 }
