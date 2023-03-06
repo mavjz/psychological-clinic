45b1d7f9b6c1bd1de3081cd9a87c99a39a97767a
mavjz28 <maja.miaskiewicz@gmail.com>
2023-03-06 11:36:02 +0100

    HeadlineType

---
+++ types/items/headline.ts
@@ -0,0 +1,12 @@
+ export type HeadlineType = {
+    h1?: boolean,
+    h2?: boolean,
+    h3?: boolean,
+    h4?: boolean,
+    h5?: boolean,
+    h6?: boolean,
+    text: string,
+    place?: string,
+    color?: string,
+    isUppercase?: boolean
+}
