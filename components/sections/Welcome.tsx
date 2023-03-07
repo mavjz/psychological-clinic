9fd6ba577f9b2758158db7c472021c44e0f0db16
mavjz28 <maja.miaskiewicz@gmail.com>
2023-03-07 12:33:17 +0100

    responsive index

--- components/sections/Welcome.tsx
+++ components/sections/Welcome.tsx
@@ -3,21 +3,38 @@ import Paragraph from "components/items/Paragraph";
 import WrapperWidth from "components/wrappers/Wrapperwidth";
 import Image from "next/image";
 import React from "react";
+import { useMedia } from "use-media";
 
 const Welcome = ({image, headline, text}: WelcomeType) => {
+    const isWide  = useMedia({minWidth: '768px'});
     return (
         <div className="welcome">
-            <WrapperWidth>
-                <div className="welcome-placeholder">
-                    <Image
-                        src={image}
-                        alt="Background photo"
-                        fill
-                        className="welcome-placeholder__image"
-                    />
-                </div>
-                <div className="welcome-gradient"/>
-            </WrapperWidth>
+            {
+                isWide ? 
+                    <WrapperWidth>
+                        <div className="welcome-placeholder">
+                            <Image
+                                src={image}
+                                alt="Background photo"
+                                fill
+                                className="welcome-placeholder__image"
+                            />
+                        </div>
+                        <div className="welcome-gradient"/>
+                    </WrapperWidth>
+                :
+                    <React.Fragment>
+                        <div className="welcome-placeholder">
+                            <Image
+                                src={image}
+                                alt="Background photo"
+                                fill
+                                className="welcome-placeholder__image"
+                            />
+                        </div>
+                        <div className="welcome-gradient"/>
+                    </React.Fragment>
+            }
             <WrapperWidth>
                 <div className="welcome-text">
                     <Headline
