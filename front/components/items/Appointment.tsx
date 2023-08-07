db7d2cc51a9e552afbbb4a9c491b12eeff415556
mavjz28 <maja.miaskiewicz@gmail.com>
2023-08-07 16:50:53 +0200

    explanation

--- front/components/items/Appointment.tsx
+++ front/components/items/Appointment.tsx
@@ -6,35 +6,35 @@ import { DayPicker } from "react-day-picker";
 import { Grid } from "react-loader-spinner";
 
 const Appointment = () => {
-    const therapist = "Bożena"
+    const TEMPtherapist = "Bożena"
+    const TEMPdate = new Date(2023, 7, 14);
     let [chosenDate, setChosenDate] = useState<Date>();
     let filters: filters = {};
     filters.therapist = {
         first_name: {
-            $eq: therapist
+            $eq: TEMPtherapist
         }
     }
     filters.date = {
-        $eq: chosenDate
+        $eq: TEMPdate
     }
     const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [appointmentsTherapist, setAppointentsTherapist] = useState<Array<any>>();
-    const [appointmentsDate, setAppointentsDate] = useState<Array<any>>();
+    const [appointmentsTherapist, setAppointmentsTherapist] = useState<Array<any>>();
+    const [appointmentsDate, setAppointmentsDate] = useState<Array<any>>();
     useEffect(() => {
         strapiAppointmentGet(filters.therapist).then((res) => {
-                setAppointentsTherapist(res.data.data);
+            console.log(res.data.data);
+                setAppointmentsTherapist(res.data.data);
                 setIsLoading(false);
             }
         );
-        strapiAppointmentGet(filters.therapist.date).then((res) => {
-                setAppointentsDate(res.data.data);
-                setIsLoading(false);
-            }
-        );
+        // strapiAppointmentGet(filters.date).then((res) => {
+        //         setAppointmentsDate(res.data.data);
+        //         setIsLoading(false);
+        //     }
+        // );
     }, [chosenDate]);
-    // TODO: aktualizowanie filtrów z nowymi zmiennymi
-    console.log(chosenDate);
-    console.log("-----------------------");
+
     if (isLoading) {
         return (
             <WrapperWidth>
@@ -53,6 +53,11 @@ const Appointment = () => {
     }
     const allDates: string[] | undefined = appointmentsTherapist?.map(item => item.attributes.date);
     const stringDate: string[] | undefined = allDates?.filter((item, index) => {return allDates?.indexOf(item) === index});
+    // deleting repeated dates 
+    /* indexOf is searching the first index numer of current item value and 
+    then compering to the left elements. If they're same, value's true, 
+    if they're reapeted value's false and won't stay in new array
+    e.g "water" is 0, but also 3, 4, 5, so only "0 water" would stay, because 0 doesn't equal 3 */
     const availableDate: Date[] | undefined = stringDate?.map(item => new Date(item));
     let allHours: Date[] | undefined = appointmentsDate?.map(item => item.attributes.time);
     let sortHours: Date[] | undefined = allHours?.sort(function (a, b) {
@@ -64,6 +69,7 @@ const Appointment = () => {
             isSameDay(day, disabledDay)
         )
     }
+    // ??? https://github.com/gpbl/react-day-picker/issues/768
     return (
         <WrapperWidth>
             <div className="appointment-content">
