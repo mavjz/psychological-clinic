ceb2e08901407d02920fc0b592e1808edf55ef28
mavjz28 <maja.miaskiewicz@gmail.com>
2023-08-14 10:55:04 +0200

    Calculator frame

--- front/components/sections/Calculator.tsx
+++ front/components/sections/Calculator.tsx
@@ -1,10 +1,35 @@
+import Button from 'components/items/Button'
 import WrapperWidth from 'components/wrappers/Wrapperwidth'
 import React from 'react'
 
 const Calculator = () => {
     return (
         <WrapperWidth>
-            
+            {/* If above 10 sessions, 15% discount
+                If has relatives, 5% discount 
+                If workshops, 1 therapy session free
+            */}
+            <div className='calculator'>
+                <form className='calculator-form'>
+                    <label htmlFor='therapist'>Wybierz terapeutę</label>
+                    <input/>
+                    <label htmlFor='session'>Podaj liczbę wizyt, które planujesz odbyć</label>
+                    <input/>
+                    <div className='calculator-form__relatives'>
+                        <label htmlFor='relatives'>Czy ktoś z Twoich bliskich korzystał z usług HumanHealth.com?</label>
+                        <input/>
+                        <label htmlFor='relativesCode'>Podaj kod wizyty bliskiej osoby</label>
+                        <input/>
+                    </div>
+                    <label htmlFor='workshop'>Warsztaty z pewności siebie</label>
+                    <input/>
+                    <Button
+                        h3
+                        text='Oblicz'
+                        className='calculator-form__submit'
+                    />
+                </form>
+            </div>
         </WrapperWidth>
     )
 }
